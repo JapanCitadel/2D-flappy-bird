438b3f51af832414ee79f9c3234e3c3ae21024ca
Thiều Thành <110987144+JapanCitadel@users.noreply.github.com>
2023-07-27 13:34:30 +0700

    Update PipeMoveScript.cs

--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -6,6 +6,7 @@ public class PipeMoveScript : MonoBehaviour
 {
     public float moveSpeed;
     public float deadZone;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +21,6 @@ public class PipeMoveScript : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
     }
 }
